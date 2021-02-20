7535b5188a8420529a56281b2fc46361d39d06c4
DV821 <66361917+DV821@users.noreply.github.com>
2021-02-20 15:00:48 +0530

    Final Deployed App

--- app.py
+++ app.py
@@ -6,22 +6,23 @@ model = pickle.load(pickle_in)
 
 
 def predict(features):
-    prediction = model.predict(features)
-    if prediction[0]==1:
-        return "Defaulter!!"
-    else:
-        return "Not A Defaulter!!"
+    prediction = model.predict(features)[0]
+    proba = model.predict_proba(features)[0]
+    output = ["Not A Defaulter", proba[1]]
+    if prediction == 1:
+        output[0] = "Defaulter"
+    return output
 
 
 def main():
-
+    # st.title("Defaulter Prediciton")
     html_temp = """
-    <div style="background-color: yellowgreen; padding : 10px">
+    <div style="background-color: tomato; padding : 10px">
     <h2 style="color:white; text-align:center;">Credit Card Defaulter Prediction</h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
-    limit_bal = st.number_input('Balance Limit', min_value=0 )
+    limit_bal = st.number_input('Balance Limit', min_value=0, step=500 )
     edu = ["1 - Graduate School", "2 - University", "3 - High School", "4 - Others"]
     mrrg = ["1 - Married", "2 - Single", "3 - Others"]
     pay1 = ["account started that month with a zero balance, and never used any credit",
@@ -37,21 +38,23 @@ def main():
         "payment delay for 8 month"]
     education = edu.index(st.selectbox( "Education", tuple(edu) ))+1
     marriage = mrrg.index(st.selectbox( "Marital Status", tuple(mrrg) ))+1
-    age = st.number_input("Age (in Years)", min_value=0)
+    age = st.number_input("Age (in Years)", min_value=8)
     pay1 = pay1.index(st.selectbox( "Pay_1", tuple(pay1) ))-2
-    bill_amt1 = st.number_input("Last month Bill Amount ", min_value=0 )
-    bill_amt2 = st.number_input("Last 2nd month Bill Amount  ", min_value=0 )
-    bill_amt3 = st.number_input("Last 3rd month Bill Amount  ", min_value=0 )
-    bill_amt4 = st.number_input("Last 4th month Bill Amount  ", min_value=0 )
-    bill_amt5 = st.number_input("Last 5th month Bill Amount  ", min_value=0 )
-    bill_amt6 = st.number_input("Last 6th month Bill Amount  ", min_value=0 )
+    bill_amt1 = st.number_input("Last month Bill Amount ", min_value=0, step=50)
+    bill_amt2 = st.number_input("Last 2nd month Bill Amount  ", min_value=0, step=50)
+    bill_amt3 = st.number_input("Last 3rd month Bill Amount  ", min_value=0, step=50)
+    bill_amt4 = st.number_input("Last 4th month Bill Amount  ", min_value=0, step=50)
+    bill_amt5 = st.number_input("Last 5th month Bill Amount  ", min_value=0, step=50)
+    bill_amt6 = st.number_input("Last 6th month Bill Amount  ", min_value=0, step=50)
 
-    pay_amt1 = st.number_input("Amount paid in Last Month  ", min_value=0 )
-    pay_amt2 = st.number_input("Amount paid in Last 2nd month  ", min_value=0 )
-    pay_amt3 = st.number_input("Amount paid in Last 3rd month  ", min_value=0 )
-    pay_amt4 = st.number_input("Amount paid in Last 4th month  ", min_value=0 )
-    pay_amt5 = st.number_input("Amount paid in Last 5th month  ", min_value=0 )
-    pay_amt6 = st.number_input("Amount paid in Last 6th month  ", min_value=0 )
+    pay_amt1 = st.number_input("Amount paid in Last Month  ", min_value=0, step=50)
+    pay_amt2 = st.number_input("Amount paid in Last 2nd month  ", min_value=0, step=50)
+    pay_amt3 = st.number_input("Amount paid in Last 3rd month  ", min_value=0, step=50)
+    pay_amt4 = st.number_input("Amount paid in Last 4th month  ", min_value=0, step=50)
+    pay_amt5 = st.number_input("Amount paid in Last 5th month  ", min_value=0, step=50)
+    pay_amt6 = st.number_input("Amount paid in Last 6th month  ", min_value=0, step=50)
+    pred = ["Predict Label", "Predict Default Rate", "Both"]
+    pred_opt = pred.index(st.selectbox( "Make Prediction of ", tuple(pred)))
 
 
     if st.button("Predict"):
@@ -60,8 +63,13 @@ def main():
         pay_amt1,pay_amt2,pay_amt3,pay_amt4,pay_amt5,pay_amt6]
         features = [np.array(features)]
         output = predict(features)
-        st.success(output)
-
+        if pred_opt == 0:
+            st.success (output[0])
+        elif pred_opt == 1:
+            st.success( f"Rate of Default : {np.round(output[1]*100,2)}%" )
+        else:
+            st.success(output[0])
+            st.success(f"Rate of Default : {np.round(output[1] * 100, 2)}%")
 
 if __name__=='__main__':
     main()
